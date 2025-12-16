fef083c42b741bc294d0690ca011f335e98cce50
Mohannad Abdallah <151975004+Mohannadx101@users.noreply.github.com>
2025-12-16 17:15:13 +0200

    Add solution for Light, more Light problem
    Implement solution for the 'Light, more Light' problem.

---
+++ UVA/Light_more_light.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: Light, more Light
+ * Author: Mohannad
+ */
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    unsigned long long n;
+    while (cin >> n && n != 0) {
+        unsigned long long root = sqrt(n);
+        if (root * root == n) {
+            cout<<"yes\n";
+        }else cout<<"no\n";
+    }
+    return 0;
+}
