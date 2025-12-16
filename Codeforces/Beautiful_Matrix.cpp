b5804444efe62144f7d6bee28e7633491e0546d8
Mohannad Abdallah <151975004+Mohannadx101@users.noreply.github.com>
2025-12-16 17:07:56 +0200

    Add Beautiful_Matrix.cpp solution
    Implement Beautiful Matrix solution to calculate moves.

---
+++ Codeforces/Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main() {
+    int mat[5][5];
+    int x,y;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> mat[i][j];
+            if (mat[i][j] == 1) {
+                x=i;
+                y=j;
+            }
+        }
+    }
+    int moves = abs(x - 2) + abs(y - 2);
+ 
+ 
+    cout<<moves;
+ 
+}
