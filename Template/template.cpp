25e713cacc71d182fa80903ad6cb9cfedc4c4479
Mohannad Abdallah <151975004+Mohannadx101@users.noreply.github.com>
2025-12-16 17:04:23 +0200

    Add template.cpp for template problem setup

---
+++ Template/template.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: templates
+ * Author: Mohannad
+ */
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+    return 0;
+}
