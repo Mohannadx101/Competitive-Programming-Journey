94f7ffc15389af533abe56fc3ea2c93f7e702fb9
Mohannad Abdallah <151975004+Mohannadx101@users.noreply.github.com>
2025-12-20 19:07:34 +0200

    Add CLion template for C++ programming

---
+++ Template/CLiontemplateV2.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem: ${FILE_NAME}
+ * Author: Mohannad
+ * Date: ${DATE}
+ */
+#[[#include <bits/stdc++.h> 
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define pb push_back
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}]]#
