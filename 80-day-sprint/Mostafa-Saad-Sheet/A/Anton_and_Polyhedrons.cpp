0ae57e6bf521f04caf9eaf37d385d66ef142c87b
Mohannad Abdallah <Mohannadx101@gmail.com>
2026-05-06 23:40:00 +0300

    improvement

--- 80-day-sprint/Mostafa-Saad-Sheet/A/Anton_and_Polyhedrons.cpp
+++ 80-day-sprint/Mostafa-Saad-Sheet/A/Anton_and_Polyhedrons.cpp
@@ -17,19 +17,16 @@ using pii = pair<int, int>;
 void solve() {
     int n;
     cin>>n;
-
-    map<string, int> shape;
-    shape.emplace("Tetrahedron", 4);
-    shape.emplace("Cube", 6);
-    shape.emplace("Octahedron", 8);
-    shape.emplace("Dodecahedron", 12);
-    shape.emplace("Icosahedron", 20);
-
+    int sum=0;
     string s;
-    int sum = 0;
-    rep(i, n) {
+
+    while (n--) {
         cin>>s;
-        sum += shape.at(s);
+        if (s[0] =='T') sum+=4;
+        else if (s[0] == 'C') sum +=6;
+        else if (s[0] == 'O') sum += 8;
+        else if (s[0] == 'D') sum += 12;
+        else if (s[0] == 'I') sum += 20;
     }
 
     cout << sum << endl;
