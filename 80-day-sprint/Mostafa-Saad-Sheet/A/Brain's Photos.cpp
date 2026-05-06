7fa23e09976a19087353a0313d82c2ef237cd500
Mohannad Abdallah <Mohannadx101@gmail.com>
2026-05-06 20:54:43 +0300

    solved:brainsphotos

---
+++ 80-day-sprint/Mostafa-Saad-Sheet/A/Brain's Photos.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem: Brain's Photos
+ * Author: Mohannad
+ * Date: 5/6/2026
+ */
+#include <bits/stdc++.h> 
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define pb push_back
+
+void solve() {
+    int n,m;
+    cin >> n >> m;
+    char mat[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> mat[i][j];
+            if (mat[i][j] == 'C'||mat[i][j] == 'M'||mat[i][j] == 'Y') {
+                cout<<"#Color"<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"#Black&White"<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
